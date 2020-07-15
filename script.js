aaf9d6f429753b3f52406917284aacf394b181fb
ocelmo <68306393+ocelmo@users.noreply.github.com>
2020-07-15 13:52:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,33 @@
 ellipse, keyCode, keyIsPressed, LEFT_ARROW, RIGHT_ARROW,*/
 
 var size;
+var increase
 
 function setup() {
   createCanvas(800, 500);
-  background(82, 222, 240);
+  background(255, 170, 0);
   size = 30;
+  increase = true;
 }
   
 function draw() {
 fill(255, 204, 0);
-ellipse(200, 298, size, size);
-
+ellipse(400, 400, size, size);
+noStroke();
 fill(76, 175, 67);
-rect(0, 500, 400, 800);
+rect(0, 400, 800, 300);
+ if(increase == true){
+  size += 1}
+
   
-size += 1
+  if(size == 500){
+    increase = false;
+    background(82,222,240)
+    fill(255, 204, 0);
+  ellipse(400, 400, size, size);
+
+  fill(76, 175, 67);
+  rect(0, 400, 800, 300);
+  }
 }
+
