37afb6d488e1e1801993379a02a53b28b11dbc09
ocelmo <68306393+ocelmo@users.noreply.github.com>
2020-07-15 12:26:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+/*global createCanvas, background, rect, fill, noStroke, width, stroke, strokeWeight, line,
+ellipse, keyCode, keyIsPressed, LEFT_ARROW, RIGHT_ARROW,*/
+
+var size;
+
+function setup() {
+  createCanvas(800, 500);
+  background(82, 222, 240);
+  size = 30;
+}
+  
+function draw() {
+fill(255, 204, 0);
+ellipse(200, 298, size, size);
+
+fill(76, 175, 67);
+rect(0, 300, 400, 120);
+
+
+}
