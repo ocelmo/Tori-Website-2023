f9945c13b898aeeb0314c897334121cfba5503f1
ocelmo <68306393+ocelmo@users.noreply.github.com>
2021-10-07 15:09:33 -0400

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,28 @@
+/*global createCanvas, background, rect, fill, noStroke, width, stroke, strokeWeight, line,
+ellipse, keyCode, keyIsPressed, LEFT_ARROW, RIGHT_ARROW, UP_ARROW, DOWN_ARROW, triangle,*/
+var xposition;
+var yposition;
+
+background(242, 193, 140);
+
+// tomato
+noStroke();
+fill(224, 90, 90);
+ellipse(150, 200, 150, 150);
+ellipse(212, 200, 150, 150);
+
+
+fill(48, 130, 31);
+rect(176, 103, 12, 32); 
+
+
+draw = function() {
+     fill(242, 193, 140);
+ellipse(mouseX, mouseY, 40, 40);
+fill(23, 163, 28);
+rect(176, 103, 12, 32); 
+
+
+};
+
+
